57e97817d70f56e10d5104e22af047cb645970b5
klayu <manuja.corp@gmail.com>
2023-09-30 13:52:15 -0700

    Saving to builder branch

--- deploy.sh
+++ deploy.sh
@@ -8,13 +8,14 @@ mkdir publicTmp
 echo "Generating site"
 npm run build
 echo "Copying to gh-pages"
+echo 'www.i94card.com' >> publicTmp/CNAME
 cp -a publicTmp/. ../gh-pages
 # cd ../gh-pages
 cd ../gh-pages && git add --all && git commit -m "gh-pages branch `date`"
 echo "Pushing to github gh-pages branch"
 git push origin gh-pages
 # HUGO_ENV=production  hugo # -t "ananke"
-echo 'www.i94card.com' >> public/CNAME
+
 
 echo "Updating builder branch"
 cd ../builder && rm -rf publicTmp && git add --all && git commit -m "Saving to builder branch"
