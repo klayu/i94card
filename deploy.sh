cf5ca093582742bdbfa989eca8b0109a314ab3d3
klayu <manuja.corp@gmail.com>
2023-09-30 13:46:40 -0700

    Saving to builder branch

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ cd ../gh-pages && git add --all && git commit -m "gh-pages branch `date`"
 echo "Pushing to github gh-pages branch"
 git push origin gh-pages
 # HUGO_ENV=production  hugo # -t "ananke"
-# echo 'www.mericanrx.com' >> public/CNAME
+echo 'www.i94card.com' >> public/CNAME
 
 echo "Updating builder branch"
 cd ../builder && rm -rf publicTmp && git add --all && git commit -m "Saving to builder branch"
