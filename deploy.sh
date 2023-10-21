69dab926f361a112db3a7562c8da4223c38f6140
klayu <manuja.corp@gmail.com>
2023-10-21 11:50:43 -0700

    Saving to builder branch

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ mkdir publicTmp
 echo "Generating site"
 npm run build
 echo "Copying to gh-pages"
-echo 'www.i94card.com' >> publicTmp/CNAME
+echo 'i94card.com' >> publicTmp/CNAME
 cp -a publicTmp/. ../gh-pages
 # cd ../gh-pages
 cd ../gh-pages && git add --all && git commit -m "gh-pages branch `date`"
